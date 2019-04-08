b2168db3104090cf27bc42309be060862d3daeca
Will <doggy.huang@gmail.com>
2019-04-08 14:01:17 +0800

    示範內嵌繫結、屬性繫結、事件繫結

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  sitename = 'Hello World';
+
+  sitelogo = '/assets/images/logo.png';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeName() {
+    this.sitename = 'The Will Will Web';
   }
 
 }
